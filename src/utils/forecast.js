772e02dbb0ea624802c6182e5f8e7f2741c0b250
Sandip Parbatrao <sandipparbatrao@Sandips-MacBook-Pro.local>
2022-10-04 16:02:53 +1100

    Updated forecast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast= (latitutde, longitude ,callback) => {
         }else if(body.error){
             callback('Location not found!')
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' celsius out. It feels like ' + body.current.feelslike + ' celsius out.' +'The humidity is '+body.current.humidity+' and chances of rains are ' +body.current.precip + '%')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' celsius out. It feels like ' + body.current.feelslike + ' celsius out.' +'The humidity is '+body.current.humidity+'% and chances of rains are ' +body.current.precip + '%')
         }
 
     })
