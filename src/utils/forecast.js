898edc69991bd438ebe7910f7a3bef377b110094
Sandip Parbatrao <sandipparbatrao@Sandips-MacBook-Pro.local>
2022-10-04 15:28:15 +1100

    Added few comments

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
 const forecast= (latitutde, longitude ,callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=5e4dcdea0277f5f9c08b6b65ab8bbbb9&query='+latitutde+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=5e4dcdea0277f5f9c08b6b65ab8bbbb9&query='+latitutde+','+longitude
 
     request({url, json: true},(error, { body }) => {
         if(error){
-            callback('Unable to connect to forecase servercs!', undefined)
+            callback('Unable to connect to forecase serveics!', undefined)
         }else if(body.error){
             callback('Location not found!')
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' faranhite out. It feels like ' + body.current.feelslike + ' faranhite out.')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' celsius out. It feels like ' + body.current.feelslike + ' celsius out.')
         }
 
     })
